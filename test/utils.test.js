9f6124a7a11a97563210413d4066ed68e61a35f5
User <user@dispostable.com>
2016-06-11 09:20:20 +0800

    fix babel encoding issue

--- test/utils.test.js
+++ test/utils.test.js
@@ -8,47 +8,47 @@ test('#ucs2decode()', (q) => {
     {
       description: 'Consecutive astral symbols',
       decoded: [127829, 119808, 119558, 119638],
-      encoded: '\uD83C\uDF55\uD835\uDC00\uD834\uDF06\uD834\uDF56',
+      encoded: [0xD83C, 0xDF55, 0xD835, 0xDC00, 0xD834, 0xDF06, 0xD834, 0xDF56],
     },
     {
       description: 'U+D800 (high surrogate) followed by non-surrogates',
       decoded: [55296, 97, 98],
-      encoded: '\uD800ab',
+      encoded: [0xD800, 0x61, 0x62],
     },
     {
       description: 'U+DC00 (low surrogate) followed by non-surrogates',
       decoded: [56320, 97, 98],
-      encoded: '\uDC00ab',
+      encoded: [0xDC00, 0x61, 0x62],
     },
     {
       description: 'High surrogate followed by another high surrogate',
       decoded: [0xD800, 0xD800],
-      encoded: '\uD800\uD800',
+      encoded: [0xD800, 0xD800],
     },
     {
       description: 'Unmatched high surrogate, followed by a surrogate pair, followed by an unmatched high surrogate',
       decoded: [0xD800, 0x1D306, 0xD800],
-      encoded: '\uD800\uD834\uDF06\uD800',
+      encoded: [0xD800, 0xD834, 0xDF06, 0xD800],
     },
     {
       description: 'Low surrogate followed by another low surrogate',
       decoded: [0xDC00, 0xDC00],
-      encoded: '\uDC00\uDC00',
+      encoded: [0xDC00, 0xDC00],
     },
     {
       description: 'Unmatched low surrogate, followed by a surrogate pair, followed by an unmatched low surrogate',
       decoded: [0xDC00, 0x1D306, 0xDC00],
-      encoded: '\uDC00\uD834\uDF06\uDC00',
+      encoded: [0xDC00, 0xD834, 0xDF06, 0xDC00],
     },
     {
       description: 'High surrogate followed by a low surrogate',
       decoded: [0x10400],
-      encoded: '\uD801\uDC00',
+      encoded: [0xD801, 0xDC00],
     },
   ];
   test('Return ucs2 code array of a string', (t) => {
     data.forEach(({ description, encoded, decoded }) => {
-      t.deepEqual(utils.ucs2decode(encoded), decoded, description);
+      t.deepEqual(utils.ucs2decode(String.fromCharCode.apply(String, encoded)), decoded, description);
     });
     t.end();
   });
